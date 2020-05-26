67d59232e71872bb5dbd8b8041936956d39f303d
Eric Johnson <johnson.n.eric@gmail.com>
2020-05-26 16:16:43 -0500

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Standard resources and paths. Tyrants has all functions, Users can't be shown in index.
+  resources :tyrants
+  resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
+
+
+  # Need to create custom route for homepage to be index of tyrants display page
 end
