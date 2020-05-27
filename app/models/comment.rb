12c6f3c787755dba525670592323dc38ad60a90a
Eric Johnson <johnson.n.eric@gmail.com>
2020-05-27 10:23:07 -0500

    rating/comment controllers and models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :tyrant
+
+    validates :user_id, presence: true
+    validates :tyrant_id, presence: true
+    validates :comments, length: { greater_than: 10 }
 end
